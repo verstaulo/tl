9cf3da363ac3e4df141520049fe1566acafb8528
alexey <verstaulo@gmail.com>
2024-08-05 20:46:23 +0300

    Fix move_speed stat

--- src/store/statsStore.js
+++ src/store/statsStore.js
@@ -9,6 +9,9 @@ export const statsStore = computed(() => {
     let result = {};
     if (equipment.value.main_weapon) {
         result = mergeObjects([DEFAULT_CHARACTER_STATS, attributesStats.value, equipmentStats.value]);
+        result.attack_speed = Number(
+            (result.attack_speed / (((result.attack_speed_bonus || 0) + 0) / 100 + 1)).toFixed(3)
+        );
         if (result.bonus_range > 0) {
             result.attack_range = Number(
                 (result.attack_range + (result.attack_range * result.bonus_range) / 100).toFixed(2)
@@ -21,9 +24,10 @@ export const statsStore = computed(() => {
             result.off_hand_weapon_attack_chance +=
                 (result.off_hand_weapon_attack_chance * result.off_hand_weapon_attack_chance_bonus) / 100;
         }
-        result.attack_speed = Number(
-            (result.attack_speed / (((result.attack_speed_bonus || 0) + 0) / 100 + 1)).toFixed(3)
-        );
+
+        if (result.move_speed_bonus > 0) {
+            result.move_speed = Number((result.move_speed * (result.move_speed_bonus / 100 + 1)).toFixed(0));
+        }
         return result;
     } else {
         result = mergeObjects([
@@ -33,6 +37,9 @@ export const statsStore = computed(() => {
             equipmentStats.value
         ]);
         result.attack_speed = Number((result.attack_speed / ((result.attack_speed_bonus || 0) / 100 + 1)).toFixed(3));
+        if (result.move_speed_bonus > 0) {
+            result.move_speed = Number((result.move_speed * (result.move_speed_bonus / 100 + 1)).toFixed(0));
+        }
     }
     return result;
 });
--- src/utils/mergeObjects.js
+++ src/utils/mergeObjects.js
@@ -5,18 +5,18 @@ const mergeObjects = (objects) => {
         if (stats) {
             Object.entries(stats).forEach(([key, value]) => {
                 if (key === 'hit') {
-                    acc.melee_hit = (acc.melee_hit ?? 0) + value;
-                    acc.ranged_hit = (acc.ranged_hit ?? 0) + value;
-                    acc.magic_hit = (acc.magic_hit ?? 0) + value;
+                    acc.melee_hit = (acc.melee_hit || 0) + value;
+                    acc.ranged_hit = (acc.ranged_hit || 0) + value;
+                    acc.magic_hit = (acc.magic_hit || 0) + value;
                 } else if (key === 'critical_hit') {
-                    acc.melee_critical_hit = (acc.melee_critical_hit ?? 0) + value;
-                    acc.ranged_critical_hit = (acc.ranged_critical_hit ?? 0) + value;
-                    acc.magic_critical_hit = (acc.magic_critical_hit ?? 0) + value;
+                    acc.melee_critical_hit = (acc.melee_critical_hit || 0) + value;
+                    acc.ranged_critical_hit = (acc.ranged_critical_hit || 0) + value;
+                    acc.magic_critical_hit = (acc.magic_critical_hit || 0) + value;
                 } else if (key === 'heavy_attack_chance') {
-                    acc.melee_heavy_attack_chance = (acc.melee_heavy_attack_chance ?? 0) + value;
-                    acc.ranged_heavy_attack_chance = (acc.ranged_heavy_attack_chance ?? 0) + value;
-                    acc.magic_heavy_attack_chance = (acc.magic_heavy_attack_chance ?? 0) + value;
-                } else acc[key] = (acc[key] ?? 0) + (value ?? 0);
+                    acc.melee_heavy_attack_chance = (acc.melee_heavy_attack_chance || 0) + value;
+                    acc.ranged_heavy_attack_chance = (acc.ranged_heavy_attack_chance || 0) + value;
+                    acc.magic_heavy_attack_chance = (acc.magic_heavy_attack_chance || 0) + value;
+                } else acc[key] = Number(((acc[key] || 0) + (value || 0)).toFixed(2));
             });
             return acc;
         } else return acc;
